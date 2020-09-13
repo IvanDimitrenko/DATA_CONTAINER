0ccecc3239dab5a7cb6e21f51396d46eb33de6c5
dimitrenko ivan <vanyadimitrenko@gmail.com>
2020-09-13 19:14:41 +0300

    tree

--- Tree/Source.cpp
+++ Tree/Source.cpp
@@ -3,7 +3,15 @@
 void main()
 {
 	Tree<int> first_tree;
-	first_tree.plant_leaf(2);
-	first_tree.plant_leaf(3);
-	
+
+
+	for (int i = 0; i < 10; i++)
+	{
+		first_tree.plant_leaf(i);
+	}
+	first_tree.show();
+	cout << "\n-----------------------------\n";
+	first_tree.binary_search(999);
+	cout << "counter >> "  << counter ;
+	cout << endl << counter << endl;
 }
--- Tree/Tree.h
+++ Tree/Tree.h
@@ -5,13 +5,17 @@
 using std::cout;
 using std::endl;
 using std::cin;
+int counter = 0;
 
-template<class T>class Tree 
+template<class T>class Tree;
+template<class T>class Leaf;
+
+template<class T>class Tree
 {
 #define PRINT_ITERATOR
 #define PRINT_TREE
-
 #define PRINT_LEAF
+
 
 	template<class T>class Leaf
 	{
@@ -22,22 +26,19 @@ template<class T>class Tree
 
 	public:
 
-		Leaf(T Data = NULL, Leaf* pLeft = nullptr, Leaf* pRight = nullptr): Data(Data),	pLeft(pLeft), pRight(pRight)
+		Leaf(T Data = NULL, Leaf* pLeft = nullptr, Leaf* pRight = nullptr) : Data(Data), pLeft(pLeft), pRight(pRight)
 		{
 #ifdef PRINT_LEAF
 			cout << "LDefGrow >> \t" << this << endl;
 #endif // PRINT_LEAF
 		}
-		
-		
+
 		~Leaf()
 		{
 #ifdef PRINT_LEAF
 			cout << "LFallOff >> \t" << this << endl;
 #endif // PRINT_LEAF
 		}
-
-
 
 	};
 	Leaf<T>* Root;
@@ -55,36 +56,74 @@ public:
 	}
 	~Tree()
 	{
-
+		destroy_tree(Root);
 		cout << "TFelling >> \t" << this;
 	}
+
+	void destroy_tree(Leaf<T>* it)
+	{
+		if (it)
+		{
+			destroy_tree(it->pLeft);
+			destroy_tree(it->pRight);
+			delete it;
+		}
+	}
+
 	void plant_leaf(T Data)		// Push_back(front) (*-*)
 	{
-		Leaf<T>* New = new Leaf<T>(Data);
-		Leaf<T>* Worm = Root;	// Iterator
-		if (size == 0)this->Root = New;	
-			else
-			{
-			//if(Worm->Data > New->Data)
-				while (/*Worm->pLeft && Worm->pRight*/ Worm)
-				{		
-					if (New->Data > Worm->Data)Worm = Worm->pLeft;
-					else Worm = Worm->pRight;			
-				}
-				Worm = New;
-			}
-			size++;
+		if (!size) this->Root = new Leaf<T>(Data);
+		else this->plant_leaf(Data, Root);
+		size++;
 	}
-	void show()		// print()
+
+	void show()const
 	{
-		
-		Leaf<T>* Worm = Root;
-		for (int i = 0; i < this->size; i++)
+		show(this->Root);
+		cout << "size >> " << this->size;
+	}
+	Leaf<T>* binary_search(T Data) { return binary_search(Data, Root); }
+	
+private:
+
+	Leaf<T>* binary_search(T Data, Leaf<T>* it)
+	{
+		counter++;
+		if (it)
 		{
-			
+			if (Data ==it->Data) { cout << endl << "---------------"   << Data << "--------------" << endl; return it; }
+
+			if (Data < it->Data) return binary_search(Data, it->pLeft);
+
+			else return binary_search(Data, it->pRight);
 		}
+		else return nullptr;
+		
+	}
 
+	void show(Leaf<T>* it)const // print()
+	{	
+		if (it) 
+		{
+			cout << it->Data << "\t";	
+			show(it->pLeft);
+			show(it->pRight);
+		}	  
+	}
 
+	void plant_leaf(T Data, Leaf<T>* it)
+	{
+		if (Data >= it->Data)
+		{
+			if (it->pRight) plant_leaf(Data, it->pRight);
+			else it->pRight = new Leaf<T>(Data);
+		}
+		else if (Data < it->Data)
+		{
+			if (it->pLeft) plant_leaf(Data, it->pLeft);
+			else it->pLeft = new Leaf<T>(Data);
+		}
+	}
 };
 
 
